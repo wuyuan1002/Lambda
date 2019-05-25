6376c28a0bd58f8f73ae781ce1c836073b6b9fed
wuyuan1002 <2461190767@qq.com>
2019-05-25 20:24:15 +0800

    Stream特点补充

--- src/stream/StreamTest.java
+++ src/stream/StreamTest.java
@@ -2,7 +2,6 @@ package stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,11 +13,19 @@ import java.util.stream.Stream;
  * @className: StreamTest
  * @description 描述:
  *
- *    流 >>> 将多个对数据的操作串联或并联起来
+ *    流 >>> 将多个对数据的操作串联或并联起来执行
  *       >>> 由三部分构成:
  *        1). 源 -- 要操作的数据，数据的来源.
  *        2). 零个或多个中间操作 -- 每个操作都是对数据的操作并且返回值也是一个流流(将当前的流转换成另外一个流)，因此可以多次地流操作.
  *        3). 一个终止操作 -- 最终的操作，得到最后的结果
+ *
+ *
+ *  流的特点：
+ *      1.流本身不存储值，它是再调用时通过管道的方式从源上获取值
+ *      2.流操作数据后会生成一个新的流，里面是修改后的数据，但流的操作不会修改数据源的数据
+ *      3.流的中间操作都是惰性的，终止操作是立即执行的。只有遇到终止操作时中间操作才会执行，如果没有终止操作的话中间操作是不会执行的
+ *      4.流的中间操作增多并不会降低效率
+ *
  *
  * @date 2019/5/25
  */
