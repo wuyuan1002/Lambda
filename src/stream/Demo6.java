0f20676aca6081870ac7ce0a162eeb80ba63b784
wuyuan1002 <2461190767@qq.com>
2019-05-29 20:28:02 +0800

    分组和分区

--- src/stream/Demo6.java
+++ src/stream/Demo6.java
@@ -3,6 +3,7 @@ package stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -28,45 +29,49 @@ public class Demo6 {
          * 按照姓名分组，组成员是list，里面是组成员对象
          */
         Map<String, List<Student>> map1 = students.stream().collect(Collectors.groupingBy(Student::getName));
-        System.out.println(map1);
 
         /*
          * 按照成绩分组，组成员是list，里面是组成员对象
          */
         Map<Integer, List<Student>> map2 = students.stream().collect(Collectors.groupingBy(Student::getScore));
-        System.out.println(map2);
 
         /*
          * 按照姓名分组，组成员long，是组中成员的个数
          */
         Map<String, Long> map3 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.counting()));
-        System.out.println(map3);
 
         /*
-         * 按照成绩分组，如果姓名相同，则按照成绩分组
+         * 先按照成绩分组，如果姓名相同，则按照成绩分组，组中成员是Student对象
          */
         Map<String, Map<Integer, List<Student>>> map4 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.groupingBy(Student::getScore)));
 
 
         /*
+         * 先按照成绩分组，如果姓名相同，则按照成绩分组，组中成员改成学生姓名放在set中
+         */
+        Map<String, Map<Integer, Set<String>>> map5 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.groupingBy(Student::getScore, Collectors.mapping(Student::getName, Collectors.toSet()))));
+
+
+        /*
          * 按照姓名分组，组成员是Double(可以是其他)，组中成员的平均分
          */
-        Map<String, Double> map5 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.averagingDouble(Student::getScore)));
-        System.out.println(map5);
+        Map<String, Double> map6 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.averagingDouble(Student::getScore)));
 
 
         /**
          * 按照成绩分区，(成绩大于60的在true区,小于六十的在false区)，区成员是list，里面是区成员对象
          */
-        Map<Boolean, List<Student>> map6 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60));
-        System.out.println(map6);
+        Map<Boolean, List<Student>> map7 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60));
 
         /**
          * 按照成绩分区，(成绩大于60的在true区,小于六十的在false区)，在同一个区的成员按照姓名分组
          */
-        Map<Boolean, Map<String, List<Student>>> map7 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60, Collectors.groupingBy(Student::getName)));
-        System.out.println(map7);
+        Map<Boolean, Map<String, List<Student>>> map8 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60, Collectors.groupingBy(Student::getName)));
 
+        /**
+         * 按照成绩分区，(成绩大于60的在true区,小于六十的在false区)，在同一个区的成员按照成绩分组，组中成员改成学生姓名放在list中
+         */
+        Map<Boolean, Map<Integer, List<String>>> map9 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60, Collectors.groupingBy(Student::getScore, Collectors.mapping(Student::getName, Collectors.toList()))));
 
     }
 }
