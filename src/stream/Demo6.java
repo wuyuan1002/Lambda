6ffe5bd49e9e8718309c267aa4be628ee9b5f1d1
wuyuan1002 <2461190767@qq.com>
2019-05-29 14:05:39 +0800

    多级分组和分区

--- src/stream/Demo6.java
+++ src/stream/Demo6.java
@@ -43,17 +43,29 @@ public class Demo6 {
         System.out.println(map3);
 
         /*
+         * 按照成绩分组，如果姓名相同，则按照成绩分组
+         */
+        Map<String, Map<Integer, List<Student>>> map4 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.groupingBy(Student::getScore)));
+
+
+        /*
          * 按照姓名分组，组成员是Double(可以是其他)，组中成员的平均分
          */
-        Map<String, Double> map4 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.averagingDouble(Student::getScore)));
-        System.out.println(map4);
+        Map<String, Double> map5 = students.stream().collect(Collectors.groupingBy(Student::getName, Collectors.averagingDouble(Student::getScore)));
+        System.out.println(map5);
 
 
         /**
          * 按照成绩分区，(成绩大于60的在true区,小于六十的在false区)，区成员是list，里面是区成员对象
          */
-        Map<Boolean, List<Student>> map5 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60));
-        System.out.println(map5);
+        Map<Boolean, List<Student>> map6 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60));
+        System.out.println(map6);
+
+        /**
+         * 按照成绩分区，(成绩大于60的在true区,小于六十的在false区)，在同一个区的成员按照姓名分组
+         */
+        Map<Boolean, Map<String, List<Student>>> map7 = students.stream().collect(Collectors.partitioningBy(stu -> stu.getScore() >= 60, Collectors.groupingBy(Student::getName)));
+        System.out.println(map7);
 
 
     }
